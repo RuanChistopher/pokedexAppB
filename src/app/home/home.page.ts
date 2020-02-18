3ba536da6cc577251e02b9f910949acbc31dbb2f
RuanChistopher <50343643+RuanChistopher@users.noreply.github.com>
2020-02-17 22:21:33 -0300

    Adcionado lista dinamica
    Atualização nos codigos dos pokemons
    Removido o HTML e criado o array com os pokemon
    Utilizado o for para exibir os dados do array.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,23 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  public listaPokemons = [
+      {numero: '001', nome:'Bulbasaur', tipos:['Grass', 'Poison'], img:'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png'},
+      {numero: '004', nome:'Charmander', tipos:['Fire'], img:'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png'},
+      {numero: '007', nome:'Squirtle', tipos:['Water'], img:'https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png'},
+      {numero: '025', nome:'Pikachu', tipos:['Eletric'], img:'https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png'},
+      {numero: '382', nome:'Kyogre', tipos:['Water'], img:'https://assets.pokemon.com/assets/cms2/img/pokedex/full/382.png'},
+      {numero: '282', nome:'Gardevoir', tipos:['Psychic', 'Fairy'], img:'https://assets.pokemon.com/assets/cms2/img/pokedex/full/282.png'},
+      {numero: '778', nome:'Mimikyu', tipos:['Ghost', 'Fairy'], img:'https://assets.pokemon.com/assets/cms2/img/pokedex/full/778.png'},
+      {numero: '093', nome:'Haunter', tipos:['Ghost', 'Poison'], img:'https://assets.pokemon.com/assets/cms2/img/pokedex/full/093.png'},
+      {numero: '172', nome:'Pichu', tipos:['Eletric'], img:'https://assets.pokemon.com/assets/cms2/img/pokedex/full/172.png'},
+      {numero: '151', nome:'Mew', tipos:['Psychic'], img:'https://assets.pokemon.com/assets/cms2/img/pokedex/full/151.png'},
+
+  ];
+
+
+  public listaFiltrada = [];
+
   constructor() {}
 
 }
